992c632e9e40c35ace7956904549aa7d79e8615b
Abdulsalam Birawi <salambirawi99@gmail.com>
2024-04-14 15:42:04 -0700

    test8

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
 
     try {
       const response = await axios.get(
-        `https://2f62-66-29-145-226.ngrok-free.app/geolocation?address=al-zahraa&email=mr.mclasher@gmail.com/geolocation?address=${address}&email=${emailAddress}`
+        `https://fc5c-66-29-145-226.ngrok-free.app/geolocation?address=${address}&email=${emailAddress}`
       );
       const data = response.data.location;
       if (data == null) {
